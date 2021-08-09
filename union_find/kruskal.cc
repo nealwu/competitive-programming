4e0a16c2c94621e4a7ecfe0baad6440f0013bf49
Neal Wu <neal@nealwu.com>
2021-08-09 14:13:51 -0500

    Add Kruskal

---
+++ union_find/kruskal.cc
@@ -0,0 +1,102 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct union_find {
+    // When data[x] < 0, x is a root and -data[x] is its tree size. When data[x] >= 0, data[x] is x's parent.
+    vector<int> data;
+    int components = 0;
+
+    union_find(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+
+    void init(int n) {
+        data.assign(n + 1, -1);
+        components = n;
+    }
+
+    int find(int x) {
+        return data[x] < 0 ? x : data[x] = find(data[x]);
+    }
+
+    int get_size(int x) {
+        return -data[find(x)];
+    }
+
+    bool unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y)
+            return false;
+
+        if (-data[x] < -data[y])
+            swap(x, y);
+
+        data[x] += data[y];
+        data[y] = x;
+        components--;
+        return true;
+    }
+};
+
+template<typename T_edge>
+struct kruskal {
+    struct edge {
+        int a, b;
+        T_edge weight;
+        int index = -1;
+        bool in_tree = false;
+
+        edge() {}
+
+        edge(int _a, int _b, T_edge _weight, int _index = -1) : a(_a), b(_b), weight(_weight), index(_index) {}
+
+        bool operator<(const edge &other) const {
+            return weight < other.weight;
+        }
+    };
+
+    union_find UF;
+    vector<edge> edges;
+    vector<bool> original_in_tree;
+
+    kruskal(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+
+    void init(int n) {
+        UF.init(n);
+        edges = {};
+    }
+
+    void add_edge(int a, int b, T_edge weight) {
+        edges.emplace_back(a, b, weight, edges.size());
+        original_in_tree.push_back(false);
+    }
+
+    template<typename T_sum>
+    T_sum solve() {
+        sort(edges.begin(), edges.end());
+        T_sum total = 0;
+
+        for (edge &e : edges)
+            if (UF.unite(e.a, e.b)) {
+                total += e.weight;
+                e.in_tree = true;
+                original_in_tree[e.index] = true;
+            }
+
+        return total;
+    }
+};
+
+
+int main() {
+
+}
