4810d7ed80ed73afe69172773960e8dbb51c6977
Neal Wu <neal@nealwu.com>
2021-08-09 14:13:19 -0500

    Add union-find implementation based on size

---
+++ union_find/union_find_size.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct union_find {
+    vector<int> parent;
+    vector<int> size;
+    int components = 0;
+
+    union_find(int n = -1) {
+        if (n >= 0)
+            init(n);
+    }
+
+    void init(int n) {
+        parent.resize(n + 1);
+        size.assign(n + 1, 1);
+        components = n;
+
+        for (int i = 0; i <= n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        return x == parent[x] ? x : parent[x] = find(parent[x]);
+    }
+
+    bool unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x == y)
+            return false;
+
+        if (size[x] < size[y])
+            swap(x, y);
+
+        parent[y] = x;
+        size[x] += size[y];
+        components--;
+        return true;
+    }
+};
+
+
+int main() {
+    ios::sync_with_stdio(false);
+#ifndef NEAL_DEBUG
+    cin.tie(nullptr);
+#endif
+
+    int N, Q;
+    cin >> N >> Q;
+    union_find UF(N);
+
+    for (int q = 0; q < Q; q++) {
+        int type, a, b;
+        cin >> type >> a >> b;
+
+        if (type == 1)
+            cout << (UF.find(a) == UF.find(b)) << '\n';
+        else if (type == 2)
+            cout << UF.unite(a, b) << '\n';
+    }
+}
